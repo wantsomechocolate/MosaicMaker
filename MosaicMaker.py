1b352215652d7b14573a9a21571a448488a96d87
wantsomechocolate <wantsomechocolate@gmail.com>
2012-10-23 11:51:32 -0300

    Update MosaicMaker.py
    changing calls to mosaicModule2 to mosaicModule. Moving around some function calls

--- MosaicMaker.py
+++ MosaicMaker.py
@@ -1,22 +1,20 @@
 import os
 import mosaicModule2    ##  My Module
 
-##  Filename
-
-progDataLog="mosaicMakerProgData.log"
-
 ##  Preliminary stuff 
 
-mosaicModule2.enterMosaic()                                         ##  Just prints welcome to Mosaic Maker
+progDataLog="mosaicMakerProgData.log"                          ##  The name of the file used to store program data
 
-mainDir=os.getcwd()                                                 ##  Get directory that program is run from.
+mainDir=os.getcwd()                                            ##  Get directory that program is run from.
 
-progDir=mosaicModule2.getProgramDirectory(mainDir,progDataLog)      ##  Check for log file and program directory stored inside. 
+##  Find or create the program directory
+
+progDir=mosaicModule.enterMosaic(mainDir,progDataLog)          ##  Check for log file and program directory stored inside. 
        
 ## Enter the program loop
 
-mosaicModule2.mosaicMakerInterface(progDir,mainDir)                 ##  Go to mosaicModule to see what this does. Aka everything. 
+mosaicModule.mosaicMakerInterface(progDir,mainDir)             ##  Go to mosaicModule to see what this does. Aka everything. 
 
 ##  Exit the program
 
-mosaicModule2.exitMosaic()                                          ##  I figured I might want something useful here one day. 
+mosaicModule.exitMosaic()                                      ##  I figured I might want something useful here one day. 
